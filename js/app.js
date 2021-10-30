b0366cb95c49a812dc316a3aa9ec421cacfe9223
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-10-30 12:38:59 +0900

    #2.2 Recap

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
+
 let painting = false;
+
+function startPainting () {
+  painting = true;
+}
+
 
 function stopPainting () {
   painting = false;
@@ -9,6 +22,15 @@ function stopPainting () {
 function onMouseMove (event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    console.log('creating')
+     ctx.lineTo(x, y);
+     ctx.stroke();
+     ctx.closePath();
+  }
 }
 
 function onMouseDown (event) {
@@ -23,7 +45,7 @@ function onMouseUp (event) {
 
 if(canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
-  canvas.addEventListener('mousedown', onMouseDown);
-  canvas.addEventListener('mouseup', onMouseUp);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 } 
